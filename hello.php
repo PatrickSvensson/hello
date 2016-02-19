d1be7242d773bedee6461407b880a7382461fd54
patrick.u.svensson@wspgroup.se <patrick.u.svensson@wspgroup.se>
2016-02-19 13:26:48 +0100

    Tag version 0.2

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 	<title>Hello World</title>
 	</head>
 	<body>
-	<hi>Hello new version is upcoming</hi>
+	<hi>Hello new version is upcoming one more time</hi>
 	<?php echo "My name is Mega"; ?>
 	</body>
 	</html>
